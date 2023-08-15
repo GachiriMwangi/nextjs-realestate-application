01937d0508c10f8964fe6cb99e9a8dffcb566236
= <=>
2023-08-15 16:31:41 +0300

    Changes

--- app/api/Appartments/route.jsx
+++ app/api/Appartments/route.jsx
@@ -1,4 +1,29 @@
 import {NextResponse} from 'next/server'
 export async function GET(request){
-    return new Response('Welcome to this page.')
+    const axios = require('axios');
+
+    const options = {
+      method: 'GET',
+      url: 'https://bayut.p.rapidapi.com/properties/list',
+      params: {
+        locationExternalIDs: '5002,6020',
+        purpose: 'for-rent',
+        hitsPerPage: '10',
+      },
+      headers: {
+        'X-RapidAPI-Key': process.env.api_key,
+        'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+      }
+    };
+    
+    try {
+        const response = await axios.request(options);
+       /* 
+       *purpose
+       */
+      const data = response.data.hits
+        return new Response(data[0])
+        } catch (error) {
+        console.error(error);
+    }
 }
--- utils/fetchApi.jsx
+++ utils/fetchApi.jsx
@@ -1,10 +1,10 @@
 import axios from 'axios' 
 const api_key = process.env.api_key
-export const baseURL = 'https://bayut.p.rapidapi.com'
+export const baseUrl = 'https://bayut.p.rapidapi.com'
 
 export const fetchApi = async(url) => {
     const {data} = await axios.get(url, {
-       headers: {    
+       headers: {
      'X-RapidAPI-Key': api_key,
          'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
       }
